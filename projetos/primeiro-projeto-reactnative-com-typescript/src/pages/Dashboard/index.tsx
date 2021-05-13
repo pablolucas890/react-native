1c9e57d6809f8a25df5d2a44cb8784e038ceed5d
pablolucas890 <pablolucas890@gmail.com>
2021-05-13 17:49:09 -0300

    add gif

--- projetos/primeiro-projeto-reactnative-com-typescript/src/pages/Dashboard/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Title } from './style';
-
-const Dashboard: React.FC = () => <Title>Explore Repositórios do GitHub</Title>;
-
-export default Dashboard;
--- projetos/primeiro-projeto-reactnative-com-typescript/src/pages/Repository/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Repository: React.FC = () => <h1>Repository</h1>;
-
-export default Repository;
